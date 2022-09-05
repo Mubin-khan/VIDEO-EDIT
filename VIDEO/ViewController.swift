550a0df9767c7376d65c88e71c16c0d2449f81df
Mubin <mubinkhan819@gmail.com>
2022-09-05 13:38:13 +0600

    Initial Commit

---
+++ VIDEO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIDEO
+//
+//  Created by appsyneefo on 9/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
